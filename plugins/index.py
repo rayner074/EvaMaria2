f84eba28ef9038138b55dff8061fa17c851e3d4a
Rayner 74 <96787752+rayner074@users.noreply.github.com>
2022-08-06 20:44:57 +0530

    Update index.py

--- plugins/index.py
+++ plugins/index.py
@@ -145,7 +145,7 @@ async def index_files_to_db(lst_msg_id, chat, msg, bot):
         try:
             current = temp.CURRENT
             temp.CANCEL = False
-            async for message in bot.get_chat_message, lst_msg_id, temp.CURRENT):
+            async for message in bot.get_chat_message(chat, lst_msg_id, temp.CURRENT):
                 if temp.CANCEL:
                     await msg.edit(f"Successfully Cancelled!!\n\nSaved <code>{total_files}</code> files to dataBase!\nDuplicate Files Skipped: <code>{duplicate}</code>\nDeleted Messages Skipped: <code>{deleted}</code>\nNon-Media messages skipped: <code>{no_media + unsupported}</code>(Unsupported Media - `{unsupported}` )\nErrors Occurred: <code>{errors}</code>")
                     break
