1b8d83fe123784cd1cb929e3ecc9accda3fde58d
Rayner 74 <96787752+rayner074@users.noreply.github.com>
2022-08-18 12:41:47 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -358,12 +358,15 @@ async def cb_handler(client: Client, query: CallbackQuery):
                 await query.answer(url=f"https://t.me/{temp.U_NAME}?start={ident}_{file_id}")
                 return
             elif AUTO_DLT == True:
-                jkl = await client.send_cached_media(
-                    chat_id=query.from_user.id,
-                    file_id=file_id,
-                    caption=f_caption,
-                    protect_content=True if ident == "filep" else False 
-                )  
+                try:   
+                    jkl = await client.send_cached_media(
+                        chat_id=query.from_user.id,
+                        file_id=file_id,
+                        caption=f_caption,
+                        protect_content=True if ident == "filep" else False 
+                    )  
+                except:
+                    return
                 await query.answer('Check PM, I have sent files in pm', show_alert=True)
                 await asyncio.sleep(30)
                 await jkl.delete()
