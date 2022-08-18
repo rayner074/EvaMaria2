10a50daa585220ad579a5cd828ea9ea3a110c992
Rayner 74 <96787752+rayner074@users.noreply.github.com>
2022-08-18 16:34:18 +0530

    Update users_chats_db.py

--- database/users_chats_db.py
+++ database/users_chats_db.py
@@ -109,13 +109,13 @@ class Database:
     async def get_settings(self, id):
         default = {
             'button': SINGLE_BUTTON,
+            'autodelete': AUTO_DLT,
             'botpm': P_TTI_SHOW_OFF,
             'file_secure': PROTECT_CONTENT,
             'imdb': IMDB,
             'spell_check': SPELL_CHECK_REPLY,
             'welcome': MELCOW_NEW_USERS,
-            'template': IMDB_TEMPLATE,
-            'autodl': AUTO_DLT
+            'template': IMDB_TEMPLATE
         }
         chat = await self.grp.find_one({'id':int(id)})
         if chat:
