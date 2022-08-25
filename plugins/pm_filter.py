1f984c253c1bee2613edea68d2f72c406c316e3a
JoKu <111365385+Joku-07@users.noreply.github.com>
2022-08-25 11:08:51 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -29,7 +29,7 @@ logger.setLevel(logging.ERROR)
 
 BUTTONS = {}
 SPELL_CHECK = {}
-AUTO_DLTS = bool(os.environ.get("AUTO_DLTS" , ""))
+AUTO_DLTS = bool(os.environ.get("AUTO_DLTS"))
 
 @Client.on_message((filters.group | filters.private) & filters.text & filters.incoming)
 async def give_filter(client, message):
